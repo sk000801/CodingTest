370fe36af225f2537262f1439876d8fc59654f66
SeoKyung Lim <83707411+Cutiepazzipozzi@users.noreply.github.com>
2023-12-10 11:28:00 +0900

    [level 3] Title: 등굣길, Time: 0.80 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n][m];
+        
+        for(int i = 0; i < puddles.length; i++) {
+            dp[puddles[i][1]-1][puddles[i][0]-1] = -1;
+        }
+        
+        dp[0][0] = 1;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(dp[i][j] == -1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+               if(i >= 1) {
+                   dp[i][j] += dp[i-1][j]%1_000_000_007;
+               }
+                if(j >= 1) {
+                    dp[i][j] += dp[i][j-1]%1_000_000_007;
+                }
+                
+                dp[i][j] %= 1_000_000_007;
+            }
+        }
+        
+        return dp[n-1][m-1]%1_000_000_007;
+    }
+}
