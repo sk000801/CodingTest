53bc903c05ab2fa7dda5788e2eca8d39b941b778
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-06-20 22:28:46 +0900

    [Silver III] Title: 1로 만들기, Time: 196 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+// 재귀로 풀면 도대체 왜 !!! 시간초과가 날까 !!!
+public class Main {
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        dp = new int[n+1];
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+1;
+
+            if(i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i/3]+1);
+            }
+
+            if(i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i/2]+1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
