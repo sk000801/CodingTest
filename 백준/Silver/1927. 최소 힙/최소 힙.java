b727efe50ed3603b32f262294aecea1b4bbb2211
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-06-22 10:19:26 +0900

    [Silver II] Title: 최소 힙, Time: 344 ms, Memory: 26272 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,29 +1,23 @@
+import java.io.*;
 import java.util.*;
 
-//최소힙
-//배열에 자연수 x를 넣음 -> 배열에서 가장 작은 값 출력 -> 배열에서 제거
-//x가 자연수라면 배열에 x라는 값을 추가
-//x가 0 -> 배열에서 가장 작은 값 출력, 배열에서 제거
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+
         StringBuffer sb = new StringBuffer();
 
-        int n = in.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i = 0; i < n; i++) {
-            int num = in.nextInt();
+            int num = Integer.parseInt(br.readLine());
 
             if(num == 0) {
-                if(pq.size() == 0) {
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(pq.poll()).append("\n");
-                }   
-            } else {
-                pq.add(num);
+                if(pq.isEmpty()) sb.append("0\n");
+                else sb.append(pq.poll()).append("\n");
             }
+            else if(num > 0) pq.add(num);
         }
 
         System.out.println(sb);
