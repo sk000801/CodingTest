8ef0e20e67b78f8e74005567f880974591f2f549
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-06-23 12:34:16 +0900

    [Silver II] Title: 에디터, Time: 848 ms, Memory: 174476 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,60 +1,49 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.*;
 
+// 결국은 iterator를 사용해야 시간 복잡도에 안걸리는 풀이
+// stack을 활용한 것은.. 포오기...
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String str = br.readLine();
-        int M = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split("");
 
-        LinkedList<Character> list = new LinkedList<Character>();
-
-        for (int i = 0; i < str.length(); i++) {
-            list.add(str.charAt(i));
+        LinkedList<String> list = new LinkedList<>();
+        for(int i = 0; i < s.length; i++) {
+            list.add(s[i]);
         }
 
-        ListIterator<Character> iter = list.listIterator();
-        while (iter.hasNext()) {
+        ListIterator<String> iter = list.listIterator();
+        while(iter.hasNext()) {
             iter.next();
         }
 
-        for (int i = 0; i < M; i++) {
-            String command = br.readLine();
-            char c = command.charAt(0);
-            switch (c) {
-                case 'L':
-                    if (iter.hasPrevious())
-                        iter.previous();
+        int command = Integer.parseInt(br.readLine());
+        for(int i = 0; i < command; i++) {
+            String[] ss = br.readLine().split(" ");
 
-                    break;
-                case 'D':
-                    if (iter.hasNext())
-                        iter.next();
+            if(ss[0].equals("P")) {
+                iter.add(ss[1]);
+            }
 
-                    break;
-                case 'B':
-                    if (iter.hasPrevious()) {
-                        iter.previous();
-                        iter.remove();
-                    }
-                    break;
-                case 'P':
-                    char t = command.charAt(2);
-                    iter.add(t);
+            if(ss[0].equals("L")) {
+                if(iter.hasPrevious()) iter.previous();
+            }
 
-                    break;
-                default:
-                    break;
+            if(ss[0].equals("D")) {
+                if(iter.hasNext()) iter.next();
+            }
+
+            if(ss[0].equals("B")) {
+                if(iter.hasPrevious()) {
+                    iter.previous();
+                    iter.remove();
+                }
             }
         }
-        for (Character chr : list) {
-            bw.write(chr);
-        }
 
-        bw.flush();
-        bw.close();
+        System.out.println(String.join("", list));
     }
 }
