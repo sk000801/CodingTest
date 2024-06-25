a06ed9a4b06aa1be17da343806fed624dd217ca3
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-06-25 22:04:40 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 252 ms, Memory: 18540 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,37 +1,26 @@
 import java.util.*;
 
-//똑같은 로직인 것 같은데 왜 주석은 안되는지 생각 좀 해보자
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        StringBuffer sb = new StringBuffer("");
-
-        // int[] dp = new int[10001];
-        // Arrays.fill(dp, 1);
-
-        // for(int i = 2; i <= 10000; i++) {
-        //     dp[i] += dp[i-2];
-        //     if(i>=3) dp[i] += dp[i-3];
-        // }
 
         int[][] dp = new int[10001][4];
         dp[1][1] = 1;
-        dp[2][1] = 1;
-        dp[2][2] = 1;
-        dp[3][1] = 1;
-        dp[3][2] = 1;
-        dp[3][3] = 1;
+        dp[2][1] = 1; dp[2][2] = 1;
+        dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1;
 
         for(int i = 4; i <= 10000; i++) {
-            dp[i][1] = 1;
-            dp[i][2] = dp[i-2][2]+dp[i-2][1];
-            dp[i][3] = dp[i-3][3]+dp[i-3][2]+dp[i-3][1];
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1]+ dp[i-2][2];
+            dp[i][3] = dp[i-3][1]+ dp[i-3][2]+ dp[i-3][3];
         }
 
         int t = in.nextInt();
+        StringBuffer sb = new StringBuffer();
+
         for(int i = 0; i < t; i++) {
             int num = in.nextInt();
-            sb.append((dp[num][1]+dp[num][2]+dp[num][3])+"\n");
+            sb.append(dp[num][1]+dp[num][2]+dp[num][3]).append("\n");
         }
 
         System.out.println(sb);
