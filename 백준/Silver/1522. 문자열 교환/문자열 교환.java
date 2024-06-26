067d728fa4e2b2f9c5bf5d83bccea1ca8aff5e52
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-06-26 20:57:20 +0900

    [Silver I] Title: 문자열 교환, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -1,8 +1,5 @@
 import java.util.*;
 
-// 인덱스의 처음부터 끝까지 시작해 a의 개수만큼 a가 연속되게 만들어줘야함
-// 그래서 b 개수가 나오면 그냥 더해주면 그게 교환 횟수임
-// 또한 문자열 원형!!! -> 나눗셈 연산으로 인덱스 오버 해결
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -10,18 +7,18 @@ public class Main {
         String s = in.nextLine();
 
         int min = Integer.MAX_VALUE;
+        int len = 0;
 
-        int aNum = 0;
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'a') aNum++;
+            if(s.charAt(i) == 'a') len++;
         }
 
         for(int i = 0; i < s.length(); i++) {
-            int bNum = 0;
-            for(int j = i; j < i+aNum; j++) {
-                if(s.charAt(j%s.length()) == 'b') bNum++;
+            int count = 0;
+            for(int j = i; j < i+len; j++) {
+                if(s.charAt(j%s.length()) == 'b') count++;
             }
-            min = Math.min(bNum, min);
+            min = Math.min(min, count);
         }
 
         System.out.println(min);
