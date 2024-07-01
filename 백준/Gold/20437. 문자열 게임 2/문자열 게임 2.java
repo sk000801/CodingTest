104942f9e2c077b6ab766e71a5915e4a3182ce70
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-01 21:40:10 +0900

    [Gold V] Title: 문자열 게임 2, Time: 532 ms, Memory: 20360 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,60 +1,63 @@
 import java.io.*;
 
-//와 투 포인터 사용했는데 그게 아니라 알파벳 별 개수를 저장하는게 좋음
-//시간초과 발생한대...
-//그리고 완전탐색 진행
-public class Main {
+// k개를 포함하는 가장 짧은, 가장 긴 문자열 찾기
+// 둘 중에 하나라도 없으면 -1 출력
+// 완전탐색하면 시간 복잡도에 걸릴 줄 알았으나 그건 아니였음 (10^6)
+
+public class Main {    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int t = Integer.parseInt(br.readLine());
 
         StringBuffer sb = new StringBuffer();
 
         for(int i = 0; i < t; i++) {
-            String w = br.readLine();
+            String s = br.readLine();   
             int k = Integer.parseInt(br.readLine());
+
+            if(k == 1) {
+                sb.append("1 1\n");
+                continue;
+            }
+
+            int[] num = new int[26];
+            for(int j = 0; j < s.length(); j++) {
+                int idx = s.charAt(j)-'a';
+                num[idx]++;
+            }
 
             int min = Integer.MAX_VALUE;
             int max = Integer.MIN_VALUE;
 
-            if(k == 1) {
-                sb.append("1 1").append("\n");
-                continue;
-            }
+            for(int j = 0; j <= s.length()-k; j++) {
+                char c = s.charAt(j);
 
-            int[] alpha = new int[26]; 
-            for(int j = 0; j < w.length(); j++) {
-                //char a가 아스키코드 97번
-                alpha[w.charAt(j)-97]++;
-            }
-
-            for(int j = 0; j <= w.length()-k; j++) {
-                char start = w.charAt(j);
-
-                if(alpha[start-97] >= k) {
+                if(num[c-'a'] >= k) {
                     int temp = 1;
-                    for(int r = j+1; r < w.length(); r++) {
-                        char cur = w.charAt(r);
-                        if(start == cur) temp++;
+                    for(int r = j+1; r < s.length(); r++) {
+                        char c2 = s.charAt(r);
+
+                        if(c == c2) temp++;
 
                         if(temp == k) {
                             min = Math.min(min, r-j+1);
                             max = Math.max(max, r-j+1);
                             break;
-                        }                        
+                        }
                     }
                 }
             }
 
-            if(min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) {
-                sb.append("-1\n");
-                continue;
+            if(min != Integer.MAX_VALUE && max != Integer.MIN_VALUE) {
+                sb.append(min).append(" ").append(max);
             } else {
-                sb.append(min).append(" ").append(max).append("\n");
-            }            
+                sb.append(-1);
+            }
+
+            sb.append("\n");
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
