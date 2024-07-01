1f4ef4a7bfb8d581117313b81b04c5485140b16b
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-01 20:51:13 +0900

    [Gold V] Title: A와 B 2, Time: 168 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,35 +1,29 @@
 import java.util.*;
 
-//set을 활용해 변형된 s의 문자열들을 담아주려고 하였으나 -> 시간초과,,,
-//t의 문자열을 하나씩 빼주면서 확인함... dfs의 방식으로?
 public class Main {
-    public static int find(String t, String s) {
-        if(t.length() == s.length()) {
-            if(t.equals(s)) return 1;
-
-            return 0;
+    static boolean isAble = false;
+    public static void check(String s, String t) {
+        if(s.equals(t)) {
+            isAble = true;
+            return;
         }
 
-        int answer = 0;
+        if(s.length() > t.length()) return;
 
+        if(t.charAt(t.length()-1) == 'A') check(s, t.substring(0, t.length()-1));
         if(t.charAt(0) == 'B') {
-            StringBuilder sb = new StringBuilder(t.substring(1));
-            answer += find(sb.reverse().toString(), s);
+            StringBuffer sb = new StringBuffer(t.substring(1, t.length()));
+            check(s, sb.reverse().toString());
         }
-
-        if(t.charAt(t.length()-1) == 'A') {
-            answer += find(t.substring(0, t.length()-1), s);
-        }
-
-        return answer > 0 ? 1 : 0;
     }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         String s = in.nextLine();
         String t = in.nextLine();
 
-        System.out.println(find(t, s));
-    }   
+        check(s, t);
+
+        System.out.println(isAble ? 1 : 0);
+    }
 }
