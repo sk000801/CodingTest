cd9df361e5c952235a8ed1c156f5b4dc419017e0
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-02 20:55:05 +0900

    [Gold V] Title: 괄호의 값, Time: 216 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,58 +1,48 @@
 import java.util.*;
 
-// 1. 올바른 괄호열인지 일단 판별
-// 2. 올바른 괄호열 뭉쳐진 애들을 분리해서 calc에 넣어줌
-// 3. 그리고 calc 값들을 더해줌
-// 50분 동안 풀어봤다
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int value = 1;
+
+        String s = in.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
         int answer = 0;
+        int value = 1;
 
-        String[] s = in.nextLine().split("");
+        StringBuffer sb = new StringBuffer();
 
-        Stack<String> stack = new Stack<>();
-        for(int i = 0; i < s.length; i++) {
-            if(s[i].equals("(")) {
-                stack.push(s[i]);
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
                 value *= 2;
-            } 
-            if(s[i].equals("[")) {
-                stack.push(s[i]);
+                stack.push(s.charAt(i));
+            } else if(s.charAt(i) == '[') {
                 value *= 3;
-            }
-            if(s[i].equals(")")) {
-                if(stack.isEmpty() || !stack.peek().equals("(")) {
+                stack.push(s.charAt(i));
+            } else if(s.charAt(i) == ')') {
+                if(stack.isEmpty() || stack.peek() != '(') { //아예 안되는 경우로 간주
                     answer = 0;
-                    System.out.println(answer);
-                    return;
-                } 
-                //근데 왜 이 조건이 붙는지 모르겠다
-                //바로 닫히는 친구들만 값을 더해준다는 건가? () [] 같은
-                else if(s[i-1].equals("(")) {
-                    answer += value;
-                }
+                    break;
+                } else if(s.charAt(i-1) == '(') answer += value;
+
                 stack.pop();
-                value /= 2;
-            }
-            if(s[i].equals("]")) {
-                if(stack.isEmpty() || !stack.peek().equals("[")) {
+                value /= 2;                
+            } else if(s.charAt(i) == ']') {
+                if(stack.isEmpty() || stack.peek() != '[') { //아예 안되는 경우로 간주
                     answer = 0;
-                    System.out.println(answer);
-                    return;
-                } else if(s[i-1].equals("[")) {
-                    answer += value;
-                }
+                    break;
+                } else if(s.charAt(i-1) == '[') answer += value;        
+                
                 stack.pop();
                 value /= 3;
             }
         }
 
-        if(!stack.isEmpty()) {
-            System.out.println(0);
-            return;
-        }
-        System.out.println(answer);
+        if(!stack.isEmpty()) sb.append(0).append("\n");
+        else sb.append(answer).append("\n");
+
+        System.out.println(sb);
     }
-}   
+}
+
