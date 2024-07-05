f8a0ebcacf33b79463187757d4d367e7dc6f8971
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-05 15:06:20 +0900

    [Gold V] Title: 용액, Time: 400 ms, Memory: 28560 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,38 +1,42 @@
 import java.io.*;
 import java.util.*;
 
+// 이분탐색밖에 방법이 없나
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
 
-        int[] water = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < water.length; i++) {
-            water[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
         }
 
-        int val = 2_000_000_001;
-
-        int[] ans = new int[2];
+        Arrays.sort(num);
 
         int start = 0;
         int end = n-1;
-        while(start < end) {
-            int res = water[start]+water[end];
 
-            //여기 절댓값 설정에서 헤맸네 이런
-            if(Math.abs(res) < val) {
-                ans = new int[]{water[start], water[end]};
-                val = Math.abs(res);
+        int min = Integer.MAX_VALUE;
+        int[] ans = new int[2];
+        while(start < end) {
+            int val = num[start]+num[end];
+
+            if(Math.abs(val) < min) {
+                min = Math.abs(val);
+                ans[0] = num[start];
+                ans[1] = num[end];
             }
 
-            if(res > 0) end--;
-            else if(res < 0) start++;
+            if(val > 0) end--;
+            else if(val < 0) start++;
             else break;
         }
 
+        Arrays.sort(ans);
+        
         System.out.println(ans[0]+" "+ans[1]);
     }
 }
