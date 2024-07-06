d25a0d73394361f2c75043c2aa7b83591608ce8d
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-07 00:05:44 +0900

    [Gold V] Title: 틱택토, Time: 212 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -1,83 +1,79 @@
 import java.util.*;
-import java.io.*;
 
-// 틱택토는 가로/세로/대각선으로 3칸을 잇는데 성공하면 즉시 끝!
-
-// 승자 정해진 여부 or O와 X의 개수
-// O와 X의 개수가 같을 때는 승자가 정해지지 않았을 때
-// X의 개수가 많을 때는 9개 꽉 채웠거나 이겼을 때
+// X와 O 순서가 번갈아 (놓여있는 개수가 X가 O 이상이어야 함)
+// 가로, 세로, 대각선 방향으로 3칸을 잇는데 성공하면 게임 끝!
+// 그냥 노가다로 풀어봐도 괜찮을 것 같은데,,,
+// 왜 안될까.. 모르겠음
+// X와 O 경우가 모두 정답을 만족할 때 for문의 순서에 따라 결과가 바뀔수도 있어서 10%에서 걸리는 것 같다
+// 경우의 수를 나눈 다음에 판별해야할듯,,
 public class Main {
-    public static boolean check(char[][] arr, char c) {
-        for(int i = 0; i < 3; i++) { //열 
-            boolean flag = true;
-            for(int j = 0; j < 3; j++) {
-                if(arr[i][j] != c) flag = false;
+    public static boolean check(char[][] ex, char c) {
+        for(int i = 0; i < 3; i++) {
+            if(ex[0][i] == ex[1][i] && ex[1][i] == ex[2][i]) {
+                if(ex[0][i] == c) return true;
             }
-            if(flag) return true;
-        }
-        
-        for(int i = 0; i < 3; i++) { //행
-            boolean flag = true;
-            for(int j = 0; j < 3; j++) {
-                if(arr[j][i] != c) flag = false;
+
+            if(ex[i][0] == ex[i][1] && ex[i][1] == ex[i][2]) {
+                if(ex[i][0] == c) return true;
             }
-            if(flag) return true;
         }
-        
-        if(arr[0][0] == c && arr[1][1] == c && arr[2][2] == c) return true;
-        if(arr[0][2] == c && arr[1][1] == c && arr[2][0] == c) return true;
-        
+
+        if(ex[0][0] == c && ex[1][1] == c && ex[2][2] == c) return true;
+        if(ex[0][2] == c && ex[1][1] == c && ex[2][0] == c) return true;
+
         return false;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        String s;
         StringBuffer sb = new StringBuffer();
 
-        while(!(s = br.readLine()).equals("end")) {
-            int countO = 0;
-            int countX = 0;
+        while(true) {
+            String s = in.nextLine();
 
-            char[][] arr = new char[3][3];
+            if(s.equals("end")) break;
+
+            char[][] ex = new char[3][3];
+            for(int i = 0; i < s.length(); i++) {
+                ex[i/3][i%3] = s.charAt(i);
+            }
+
+            int xCount = 0;
+            int oCount = 0;
+
             for(int i = 0; i < 3; i++) {
                 for(int j = 0; j < 3; j++) {
-                    arr[i][j] = s.charAt(3*i+j);
-                    if(arr[i][j] == 'O') countO++;
-                    if(arr[i][j] == 'X') countX++;
+                    if(ex[i][j] == 'X') xCount++;
+                    if(ex[i][j] == 'O') oCount++;
                 }
             }
 
-            int diff = countX-countO;
-            if(diff == 0 || diff == 1) {
-                if(countX >= 3) {
-                    if(check(arr, 'X')) {
-                        if(check(arr, 'O')) sb.append("invalid\n");
-                        else if(diff == 1) sb.append("valid\n");
-                        else sb.append("invalid").append("\n");       
-                        
-                        continue;
-                    }
-                }
-                if(countO >= 3) {
-                    if(check(arr, 'O')) {
-                        if(diff == 0) sb.append("valid").append("\n");
-                        else sb.append("invalid").append("\n");
-
-                        continue;
-                    }
-                }
-                if(countX+countO < 9) {
-                    sb.append("invalid\n");
-                    continue;
-                }
-            } else {
+            if(!(xCount == oCount || xCount - oCount == 1)) {
                 sb.append("invalid\n");
                 continue;
             }
 
-            sb.append("valid\n");
+            if(xCount+oCount == 9 && xCount-oCount == 1) {
+                if(!check(ex, 'O')) {
+                    sb.append("valid\n");
+                    continue;
+                }
+            } else {
+                if(check(ex, 'X') && xCount-oCount == 1) {
+                    if(!check(ex, 'O')) {
+                        sb.append("valid\n");
+                        continue;
+                    }
+                }
+
+                if(check(ex, 'O') && xCount == oCount) {
+                    sb.append("valid\n");
+                    continue;
+                }
+            }
+
+            sb.append("invalid\n");
         }
 
         System.out.println(sb);
