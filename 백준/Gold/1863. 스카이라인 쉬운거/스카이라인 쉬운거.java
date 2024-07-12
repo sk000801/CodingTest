1287ec7fc147fcfcd8b90dc6daba40eaf24eaa05
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-12 21:17:08 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 180 ms, Memory: 19636 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -6,6 +6,7 @@ import java.util.*;
 // -> 높이가 같다면 같은 건물로 취급하고 continue
 // -> 입력값이 더 크다면 뒤의 빌딩과 이어지는 새로운 건물일수도
 // -> 입력값이 더 작다면 이전의 빌딩들이 하나의 건물임이 입증
+// 풀이 보고 30분 절대 안쉬운데요..? 높이만 보고 비교하는 줄 몰랐음
 public class Main {    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
