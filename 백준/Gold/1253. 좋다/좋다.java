654010988bfb7778cd61422d04871ce01418073b
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-14 09:06:48 +0900

    [Gold IV] Title: 좋다, Time: 176 ms, Memory: 15140 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,39 +1,40 @@
+import java.io.*;
 import java.util.*;
 
-// 왜 자꾸 틀리나 했더니 if문으로 continue 안해줘서 그런듯 
+// 처음에는 특정 인덱스의 범위(0, i보다 작은)로 탐색했으나
+// 그냥 전체 인덱스의 범위를 탐색
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = in.nextInt();
-        int[] arr = new int[n];
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
+            num[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(num);
+
         int answer = 0;
-
-
         for(int i = 0; i < n; i++) {
-            int num = arr[i];
+            int temp = num[i];
+            int idx = 0;
+            int idx2 = n-1;
+            while(idx < idx2) {
+                if(idx == i) idx++;
+                else if(idx2 == i) idx2--;
+                if(idx >= idx2) break;
 
-            int left = 0;
-            int right = n-1;
-
-            while(left < right) {
-                if(left == i) left++;
-                else if(right == i) right--;
-
-                if(left >= right) break;
-
-                if(arr[left]+arr[right] < num) left++;
-                else if(arr[left]+arr[right] > num) right--;
+                int value = num[idx]+num[idx2];
+                if(value > temp) idx2--;
+                else if(value < temp) idx++;
                 else {
                     answer++;
                     break;
-                }                
-            }         
+                }
+            }
         }
 
         System.out.println(answer);
