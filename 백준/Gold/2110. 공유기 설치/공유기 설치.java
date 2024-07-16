dd48b96397673d7a011b24c847515ec0439907dc
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-16 22:11:50 +0900

    [Gold IV] Title: 공유기 설치, Time: 308 ms, Memory: 29368 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,51 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+// 이건 다시 봐도 못 풀겠음 ㅠㅠ
 public class Main {
-    static int n, c;
-    static int[] house;
-    public static int check(int distance) {
+    public static int cal(int dist, int[] japyo, int n) {
         int count = 1;
 
-        int temp = house[0];
+        int temp = japyo[0];
         for(int i = 1; i < n; i++) {
-            if(temp+distance <= house[i]) {
+            if(temp+dist <= japyo[i]) {
                 count++;
-                temp = house[i];
+                temp = japyo[i];
             }
         }
 
         return count;
     }
-    public static int search(int start, int fin) {
-        while(start < fin) {
-            int mid = (start+fin)/2;
 
-            if(check(mid) < c) {
-                fin = mid;
-            } else {
-                start = mid+1;
-            }
-        }
-
-        //반환값이 start < fin 조건을 초과하는 값이므로 1을 빼야함
-        return start-1;
-    }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        
-        house = new int[n];
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            house[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(house);
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        //집 사이의 거리를 이분탐색의 단위로
-        System.out.println(search(1, house[n-1]-house[0]+1));
+        int[] japyo = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            japyo[i] = Integer.parseInt(br.readLine());
+        }   
+        Arrays.sort(japyo);
+
+        int start = 0;
+        int fin = japyo[n-1]-japyo[0]+1;
+        while(start < fin) { //start >= fin인 상태에서 while문이 끝남
+            int mid = (start+fin)/2;
+            if(cal(mid, japyo, n) < c) fin = mid;
+            else start = mid+1;
+        }
+
+        System.out.println(start-1);
     }
 }
