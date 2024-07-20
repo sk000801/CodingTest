ddb038f1bc133397ff90313925c609cc40b161da
SeoKyung Lim <83707411+sk000801@users.noreply.github.com>
2024-07-20 12:30:51 +0900

    [Silver I] Title: 트럭, Time: 280 ms, Memory: 21016 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,39 +1,40 @@
 import java.util.*;
 
-//정답: 최단시간
+// 동시에 w대의 트럭만
+// 무게 l 초과하면 안됨
+// q에 미리 w대만 넣어놓고 빼고 넣으면서 합 비교
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt(); //트럭개수
-        int w = in.nextInt(); //다리길이
-        int l = in.nextInt(); //최대하중
-        int[] truck = new int[n];
-        
+        int n = in.nextInt();
+        int w = in.nextInt();
+        int l = in.nextInt();
+
+        int[] weight = new int[n];
+
         for(int i = 0; i < n; i++) {
-            truck[i] = in.nextInt();
+            weight[i] = in.nextInt();
         }
 
         Queue<Integer> q = new LinkedList<>();
+
         for(int i = 0; i < w; i++) {
             q.add(0);
         }
 
         int answer = 0;
         int idx = 0;
-        int weight = 0;
+        int hap = 0;
         while(!q.isEmpty()) {
             answer++;
-            weight -= q.poll();
+            hap -= q.poll();
 
-            if(idx >= truck.length) continue;
-
-            if(weight+truck[idx] <= l) {
-                weight += truck[idx];
-                q.add(truck[idx]);
-                idx++;
-            } else {
-                q.add(0); //다리 건너는 시간 or 공백
+            if(idx < n) {
+                if(weight[idx]+hap <= l) {
+                    hap += weight[idx];
+                    q.add(weight[idx++]);
+                } else q.add(0);
             }
         }
 
